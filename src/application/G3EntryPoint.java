e4ba57fa760d8f465c745fbb263a01c91f66a711
janam1003 <lucasjanamsmile@gmail.com>
2024-01-31 10:54:02 +0100

    merging

--- src/application/G3EntryPoint.java
+++ src/application/G3EntryPoint.java
@@ -1,53 +1,58 @@
 package application;
 
+import encryption.EncryptionImplementation;
+import entities.Customer;
+import entities.EnumTripType;
+import entities.EnumUserType;
+import entities.Trip;
+import entities.TripInfo;
+import entities.TripInfoId;
+import entities.User;
+import exception.CreateException;
+import exception.ReadException;
+import factories.CustomerManagerFactory;
+import factories.UserManagerFactory;
+import interfaces.CustomerManager;
+import interfaces.UserManager;
+import java.util.Date;
+import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.application.Application;
-import static javafx.application.Application.launch;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.stage.Stage;
-import view.signin.LoginController;
+import restful.CustomerRESTClient;
+import restful.TripInfoRESTClient;
+import restful.TripRESTclient;
+import restful.UserRESTClient;
 
-/**
- * Starting point of an application
- *
- * @author Janam
- */
-public class G3EntryPoint extends Application {
-
-    /**
-     * Logger object used to log messages for the application.
-     */
-    private static final Logger LOGGER = Logger.getLogger(Application.class.getName());
-
-    /**
-     * Open the login window
-     *
-     * @param stage Stage where the scene will be projected
-     * @throws Exception all exceptions
-     */
-    @Override
-    public void start(Stage stage) throws Exception {
-
-        LOGGER.info("Initializing start method to open signin window.");
-
-        // Load the FXML file
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/signin/Login.fxml"));
-
-        // Get the root
-        Parent root = (Parent) loader.load();
-
-        // Get the controller
-        LoginController controller = (LoginController) loader.getController();
-
-        // Set the stage
-        controller.setStage(stage);
-
-        // Initialize the stage
-        controller.initStage(root);
-    }
+public class G3EntryPoint {
 
     public static void main(String[] args) {
-        launch(args);
-    }
+        // Create instances of your REST clients
+        CustomerRESTClient customerClient = new CustomerRESTClient();
+        CustomerManager mang = CustomerManagerFactory.getCustomerManager();
+        UserManager manU = UserManagerFactory.getUserManager();
+        User user = new User();
+        User user2 = new User();
+
+
+        try {
+            Customer cust= new Customer();
+            cust.setMail("porfaporfa1@mail.com");
+            cust.setUserType(EnumUserType.CUSTOMER);
+            cust.setPassword("uwu");
+            cust.setCreationDate(null);
+            try {
+                mang.createCustomer(cust);
+            } catch (CreateException ex) {
+                Logger.getLogger(G3EntryPoint.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            user2.setMail("porfaporfa1@mail.com");
+            user2.setPassword("uwu");
+            
+            
+            user=manU.signIn(user2);
+            System.out.println("PORFAA USER DEVUELTO: " + user);
+        } catch (ReadException ex) {
+            Logger.getLogger(G3EntryPoint.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
 }
+    }
--- src/application/package-info.java
+++ src/application/package-info.java
@@ -1,4 +1,1 @@
-/**
- * This is the package where the application starts.
- */
 package application;
